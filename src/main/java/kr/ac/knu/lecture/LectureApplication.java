f1ac89210c3cfeadd8a87f6312948bc3cc06b444
rokim <rokim@riotgames.com>
2018-11-16 13:37:46 +0900

    Initialize project

---
+++ src/main/java/kr/ac/knu/lecture/LectureApplication.java
@@ -0,0 +1,12 @@
+package kr.ac.knu.lecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LectureApplication.class, args);
+	}
+}
